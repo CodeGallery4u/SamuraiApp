39347780f90f77da6b19a60b9b425e7f4e32b8e3
SIDDIQUI <mohdsafderuddin.siddiqui@t-systems.com>
2021-11-18 17:00:02 +0530

    added connection stirng specific setting

---
+++ SamuraiApp.Data/SamuraiContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SamuraiApp.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SamuraiApp.Data
+{
+    public class SamuraiContext : DbContext
+    {
+        public DbSet<Samurai> Samurai { get; set; }
+        public DbSet<Quote> Quote { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("(localdb)\\MSSQLLocalDB; Initial Catalog=SamuraiAppData");
+        }
+    }
+}
